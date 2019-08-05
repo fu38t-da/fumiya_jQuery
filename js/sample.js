2eea635c1c2e583335036a239b2391aaa33c3b3a
fumiya <fu38.terada@gmail.com>
2019-08-05 16:56:15 +0900

    first commit
    HTML、CSS、JSファイルを追加

---
+++ js/sample.js
@@ -0,0 +1,45 @@
+$(function(){
+  //ボタンサイズの変更
+  $('#js-click-btn').on('click',function(){
+    $(this).addClass('large-btn');
+  })
+
+  //ボタンの色変更
+  $('#js-hover-btn').mouseover(function(){
+    $(this).addClass('opacity');
+  }).mouseleave(function(){
+    $(this).removeClass('opacity');
+  });
+  // console.log($('#js-hover-btn'));
+
+  $('#js-show-btn').on('click',function(){
+    // ミリ秒
+    // slideDownも可能
+    $('#js-target-element').fadeIn(2000);
+  });
+
+  $('#js-hide-btn').on('click',function(){
+    $('#js-target-element').fadeOut(2000);
+  });
+
+  $('#js-add-btn').on('click',function(){
+    // $(this).before('<li class="btn">ボタンの前に追加</li>');
+    $(this).after('<li class="btn">ボタンの後ろに追加</li>');
+  });
+
+  //要素の前後(中)に追加
+  $('#js-add-btn2').on('click', function() {
+    // $(this).prepend('<li>前</li>');
+    $(this).append('<li>後</li>');
+  });
+
+  $('.js-toggle').on('click', function(){
+    $(this).toggleClass('on');
+    $(this).siblings().slideToggle();
+  })
+
+});
+
+// jQueryは配列として情報を取得している
+// （console.logで調べるとわかる）
+//　親子関係を考えるとわかりやすい
